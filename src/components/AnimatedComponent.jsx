7a7e2d15423e5e4d681903788a56b053977428d4
Aditya Phasu <adityaphasu919@gmail.com>
2023-07-15 17:19:52 +0530

    refactor: shouldAnimate logic

--- src/components/AnimatedComponent.jsx
+++ src/components/AnimatedComponent.jsx
@@ -4,7 +4,7 @@ const AnimatedComponent = ({ tag, variants, ...otherProps }) => {
   const MotionTag = motion[tag];
   const prefersReducedMotion = useReducedMotion();
 
-  const shouldAnimate = true ? "visible" : variants;
+  const shouldAnimate = prefersReducedMotion ? "visible" : variants;
 
   return (
     <MotionTag viewport={{ once: true, threshold: 1 }} variants={shouldAnimate} {...otherProps}></MotionTag>
